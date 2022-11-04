b9493ebd6730428ac45cfa5da7e9f30dc8b184ee
SiddharthGanesh2001 <siddharthganesh2001@gmail.com>
2022-11-04 18:39:02 +0530

    0.2

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-// document.addEventListener("submit", submitHandler);
-
-// const submitHandler=function(event) {
-//     let task=document.querySelector("textarea").value
-//     console.log(task);
-// }
 let taskBox=[];
 let ul=document.createElement("ul");
-document.querySelector("#tasks").appendChild(ul);
+document.querySelector("#taskBox").appendChild(ul);
 const addButton=document.querySelector("#add");
-
-
 
 const clickHandler=function(event) {
     const task=new Object();
-
     task.title=document.querySelector("#addTaskBox").value;
     task.status="pending";
-    taskBox.push(task);
-    console.log(taskBox);
-    let taskHTML=getTaskHTMLContent(task);
-    document.querySelector("#addTaskBox").value="";
-    renderTaskBox(taskHTML);
-    
+    if(task.title){
+        taskBox.push(task);
+        console.log(taskBox);
+        let taskHTML=getTaskHTMLContent(task);
+        document.querySelector("#addTaskBox").value="";
+        renderTaskBox(taskHTML);
+    }
 }
 addButton.addEventListener("click", clickHandler);
 
-
 function getTaskHTMLContent(task) {
     let taskHTML=`<li>
-                    <input type="checkbox" name="${task.title}" value="${task.title}"/>
+                    <input type="checkbox" name="${task.title}" id="${task.title}"/>
                     <label for="${task.title}">${task.title}</label>
-                </li>`
+                    <span id="removeButton">
+                        <button><strong>X</strong></button>
+                    </span><br><br>
+                </li>`;
     console.log(taskHTML);
     return taskHTML;
 }
+
 
 function renderTaskBox(taskHTML) {
     ul.insertAdjacentHTML("beforeend",taskHTML);
